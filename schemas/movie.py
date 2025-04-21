46344805250b80619c38a1fca1276307e5f2346b
Mohamed Harby <mohamed.harby.ce@gmail.com>
2025-04-21 19:34:07 +0200

    feat: fetched-genres-names-using-id

--- schemas/movie.py
+++ schemas/movie.py
@@ -4,6 +4,6 @@ from typing import Optional, List
 class Movie(BaseModel):
     title: str
     year: Optional[str]
-    genre: Optional[List[str]]
+    genres: Optional[List[str]]
     poster_url: Optional[str]
     source: str
